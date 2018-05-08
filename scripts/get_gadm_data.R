b9ac7d50cdaa8d0b4cdd88f77ced79ab135d6237
lecrabe <remi.dannunzio@gmail.com>
2018-05-08 16:53:56 +0700

    khyov region

--- scripts/get_gadm_data.R
+++ scripts/get_gadm_data.R
@@ -16,9 +16,6 @@ library(ggplot2)
 library(rgdal)
 
 ## Set the working directory
-workdir  <- "/media/dannunzio/OSDisk/Users/dannunzio/Documents/countries/cambodia/workshop_20180507/"
-setwd(workdir)
-gadm_dir <- paste0(rootdir,"data/gadm/")
 
 ## Get List of Countries and select only Cambodia
 (gadm_list  <- data.frame(getData('ISO3')))
@@ -34,13 +31,15 @@ writeOGR(aoi,
          overwrite_layer = T)
 
 ## Select one province and export as KML
-sub_aoi <- aoi[aoi$NAME_4 == "Samret",]
+#sub_aoi <- aoi[aoi$NAME_4 == "Samret",]
+sub_aoi <- aoi[aoi$NAME_4 == "Khyov",]
 
 plot(getData('GADM',path=gadm_dir , country= countrycode, level=1))
 plot(sub_aoi,add=T,col="red")
 
 sub_aoi@data <- sub_aoi@data[,c("OBJECTID","ISO")]
-writeOGR(sub_aoi,paste0(gadm_dir,"work_aoi.kml"),"work_aoi","KML",overwrite_layer = T)
+writeOGR(sub_aoi,paste0(gadm_dir,"work_aoi_khyov.kml"),"work_aoi_khyov","KML",overwrite_layer = T)
 
 ## Load inside Google Drive as a Fusion Table and note that FT ID
-my_fusion_table_id <- "1EiIBGwFKpZrlItVgkM01c4AOQ5Ms-MhIB8kbatBk"
+#my_fusion_table_id <- "1EiIBGwFKpZrlItVgkM01c4AOQ5Ms-MhIB8kbatBk"
+my_fusion_table_id <- "1Y9AxDnjeTKpHrmnNRsT9wnZOumb8vX9vccc-l-Ik"
