20c71d40fbc7a73df29af1a3fa31a9441d324b56
lecrabe <remi.dannunzio@gmail.com>
2018-05-10 13:18:22 +0700

    install GFC analysis

--- scripts/s0_parameters.R
+++ scripts/s0_parameters.R
@@ -9,6 +9,15 @@
 ####################################################################################################
 options(stringsAsFactors = FALSE)
 
+packages <- function(x){
+  x <- as.character(match.call()[[2]])
+  if (!require(x,character.only=TRUE)){
+    install.packages(pkgs=x,repos="http://cran.r-project.org")
+    require(x,character.only=TRUE)
+  }
+}
+packages(gfcanalysis)
+
 ### Load necessary packages
 library(raster)
 library(rgeos)
